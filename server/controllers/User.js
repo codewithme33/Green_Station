caaaffcc116abcd32b2502f0db53085bfaf7a560
codewithme33 <codewithme33@gmail.com>
2025-02-09 20:48:06 +0545

    authentication add

---
+++ server/controllers/User.js
@@ -0,0 +1,40 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("./models/User"); // Your User model
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      // Check if user already exists in your database
+      let user = await User.findOne({ googleId: profile.id });
+
+      if (!user) {
+        // Create a new user if they don't exist
+        user = new User({
+          googleId: profile.id,
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          avatar: profile.photos[0].value,
+        });
+        await user.save();
+      }
+
+      done(null, user);
+    }
+  )
+);
+
+// Serialize and deserialize user
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
